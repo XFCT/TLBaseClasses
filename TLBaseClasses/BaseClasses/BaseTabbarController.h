7bf6ee9f3c2b58d3ebd980876f0dccc0117e459c
小风车 <1016042696@qq.com>
2019-04-30 17:02:11 +0800

    fix

--- TLBaseClasses/BaseClasses/BaseTabbarController.h
+++ TLBaseClasses/BaseClasses/BaseTabbarController.h
@@ -2,8 +2,8 @@
 //  BaseTabbarController.h
 //  TLBaseClasses
 //
-//  Created by 五月同辉 on 2018/3/20.
-//  Copyright © 2018年 五月同辉. All rights reserved.
+//  Created by TL on 2018/3/20.
+//  Copyright © 2018年 TL. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
